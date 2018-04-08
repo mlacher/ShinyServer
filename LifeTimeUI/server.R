2dbc7cfbe1f91b7c4e3a183411c247af1e9b7a5d
mlacher <mlacher@live.de>
2018-04-08 16:09:33 +0200

    Update

--- LifeTimeUI/server.R
+++ LifeTimeUI/server.R
@@ -1,63 +1,47 @@
-#
+
+
 # This is the server logic of a Shiny web application. You can run the
+
 # application by clicking 'Run App' above.
+
 #
+
 # Find out more about building applications with Shiny here:
+
 #
-#    http://shiny.rstudio.com/
-#
+
+#    http://shiny.rstudio.com
+
 
 library(shiny)
-library(LTSerial)
-library(car)
-
-
-Device<-Open_Serial("COM31")
-
-RWout<-RW_Serial(Device,'276')
-if (RWout == 20)
-{
-  abc = 0
-}else{
-  abc = NA
-}
-
+stpt <- Sys.time()+200
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
 
-  st <- reactiveValues(flag = 0, Breath  = 0)
-  observeEvent(input$start, {
-       st$flag <- 1
- })
+shinyServer(function(input, output, session) {
+  
+  st <- reactiveValues(flag = 0, 
+                       Breath  = 0)
+  
+  observeEvent(input$start,{st$flag<-1})
+  
+  observeEvent(input$pause,{st$flag<-0})
 
-  observeEvent(input$stop, {
-      st$flag <- 0
-  })
-
-  observeEvent(st$flag,{
+  observeEvent(input$stop,{st$Breath<-0 
+                             st$flag<-0})
+ 
+  
+  observe({
     if(st$flag==1){
-      print(st$flag)
-      InhTime<-input$slider1*10
-      InhTimec<- as.character(InhTime+255+1)
-      print(InhTimec)
-      RWout<-RW_Serial(Device,'517')
-      Rout <- R_Serial(Device)
-      st$Data <- Meas_Data(Rout)
-      st$Breath<-st$Breath+1
-      st$Wait<- 1
-      st$flag <-0
-     }
+      invalidateLater(input$slider2*1000)}
+    print(input$slider2)
+    isolate({
+      st$Data <- mtcars
+      st$Breath<-st$Breath+1})
   })
-
-
-
-    output$value3 <- renderPrint({ st$Breath})
-    output$plot <- renderPlot({plot(st$Data$Time, st$Data$RotSpeed )})
-    output$summary <- renderPrint({summary(st$Data)})
-    output$table <- renderTable({data
-      st$Wait<- 0
-      st$flag<-1})
-
-
+  
+  output$value3 <- renderPrint({ st$Breath})
+  output$plot <- renderPlot({plot(st$Data$mpg[1:st$Breath], st$Data$cyl[1:st$Breath])})
+  output$summary <- renderPrint({summary(st$Data)})
+  output$table <- renderTable({data})
 
 })
