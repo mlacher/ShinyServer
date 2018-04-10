c08f486e9be14f0a21e2f4acdab74e38853ed3c0
mlacher <mlacher@live.de>
2018-04-10 15:26:54 +0200

    update with data recording

--- LifeTimeUI/server.R
+++ LifeTimeUI/server.R
@@ -8,25 +8,28 @@ Device<-Open_Serial("COM31")
 shinyServer(function(input, output, session) {
 
   st <- reactiveValues(flag = 0,
-                       Breath  = 0)
+                       Breath  = 0,
+                       Data1 = 0)
 
   observeEvent(input$start,{st$flag<-1
   st$ExhTime<- input$slider2
-  st$InhTime<-input$slider1})
+  st$InhTime<-input$slider1
+  st$TotTime<-st$ExhTime+st$InhTime})
   observeEvent(input$pause,{st$flag<-0})
   observeEvent(input$stop,{st$Breath<-0
                              st$flag<-0})
 
   observe({
     if(st$flag==1){
-      invalidateLater((st$ExhTime+st$InhTime)*1000)
+      invalidateLater(st$TotTime*1000)
     isolate({
       print(st$ExhTime)
       InhTimec<- as.character( (st$InhTime*10)+255+1)
       RWout1<-RW_Serial(Device,InhTimec)
       RWout2<-RW_Serial(Device,'517')
-      Rout <- R_Serial(Device, ceiling((st$ExhTime+st$InhTime)))
+      Rout <- R_Serial(Device, ceiling(st$TotTime))
       st$Data <- Meas_Data(Rout)
+      st$Data1 <- cbind(st$Data1,st$Data$RotSpeed)
       print(st$InhTime)
       st$Breath<-st$Breath+1})}
   })
@@ -34,7 +37,8 @@ shinyServer(function(input, output, session) {
   output$value3 <- renderPrint({ st$Breath})
   output$plot <- renderPlot({plot(st$Data$Time, st$Data$RotSpeed)})
   output$summary <- renderPrint({summary(st$Data)})
-  output$table <- renderTable({data})
+  output$table <- renderTable({
+    st$Data1})
 
 })
 
